f4ac8dccfe58e46c7e053e4be0c9ddae620c8a61
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-02-19 22:48:36 +0200

    add work "oop virtual files"

---
+++ oop_virtural_files-main/oop_virtual_files/oop_virtual_files/oop_virtual_files.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <bitset>
+#include <iostream>
+using namespace std;
+
+class File {
+private:
+	string name = "";
+protected:
+	fstream file;
+	
+public:
+	File() : name("text.txt") {
+	}
+	File(string name) : name(name) {
+		
+		file.open(name);
+		
+	}
+	~File()
+	{
+		file.close();
+	}
+
+	void Write() {
+
+		const int SIZE = 100;
+		char text[SIZE];
+		cout << "Input new text to file >> ";
+		cin.getline(text, SIZE);
+
+		file << text << endl;
+
+	}
+
+	void Clear() {
+		file.close();
+		file.open(name, ios_base::trunc);
+		cout << "File was clear!\n";
+	}
+
+	virtual void Display() {
+		file.close();
+		file.open(name);
+		cout << "\nFile contant: \n";
+		
+		string line = "";
+
+		while (!file.eof()) {
+
+			getline(file, line);
+			cout << line << endl;
+
+		}
+	}
+};
+
+class ASCIIFile : public File {
+public:
+	ASCIIFile(string name) : File(name){
+
+	}
+
+	void Display() override {
+		cout << "\nFile contant with ASCII: \n";
+
+		char sym;
+		while (!file.eof()) {
+			file.get(sym);
+
+			if (!file.eof()) {
+				cout << int(sym) << " ";
+			}
+			else {
+				break;
+			}
+		}
+		cout << endl;
+	}
+
+};
+
+
+
+class Double : public File {
+public:
+	Double(string name) : File(name) {
+
+	}
+
+	void Display() override {
+		cout << "\nFile contant with binary code: \n";
+
+		char sym;
+		while (!file.eof()) {
+			file.get(sym);
+			bitset<8> b(sym);
+
+			if (!file.eof()) {
+				cout << b << " ";
+			}
+			else {
+				break;
+			}
+		}
+		cout << endl;
+
+	}
+};
+
+int main()
+{
+	
+	File f("file.txt");
+
+	f.Write();
+	f.Write();
+
+	f.Display();
+
+	ASCIIFile s("file.txt");
+
+	s.Display();
+
+	Double a("file.txt");
+
+	a.Display();
+}
