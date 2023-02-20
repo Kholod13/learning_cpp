260e6ea4822e4b54667a2e64cdda9d2b39db9ac1
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-02-20 18:05:26 +0200

    add work "oop_aviaTickets"

---
+++ oop_aviaTickets/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+struct Ticket {
+    int ser;
+    int num;
+    int y;
+    int m;
+    int d;
+    string placeDeparture;
+    string placeArrival;
+};
+
+class AviaTickets {
+private:
+    vector<Ticket> tickets;
+
+    void ReviewOne(int n) {
+        cout << "Ticket serial: " << tickets[n].ser << endl;
+        cout << "Ticket number: " << tickets[n].ser << endl;
+        cout << "Time:\nyear: " << tickets[n].ser << endl;
+        cout << "month: " << tickets[n].ser << endl;
+        cout << "day: " << tickets[n].ser << endl;
+        cout << "Place departure: " << tickets[n].ser << endl;
+        cout << "Place arrival: " << tickets[n].ser << endl;
+    }
+
+public:
+    AviaTickets()
+    {
+
+    }
+
+    void Add() {
+        
+        srand(unsigned(time(0)));
+
+        Ticket t;
+
+        t.ser = rand() % 100;
+        t.num = rand() % 100000;
+
+        cout << "Input year >> ";
+        cin >> t.y;
+        cout << "Input month >> ";
+        cin >> t.m;
+        cout << "Input day >> ";
+        cin >> t.d;
+        cout << "Input place departure >> ";
+        cin >> t.placeDeparture;
+        cout << "Input place arrival >> ";
+        cin >> t.placeArrival;
+
+        tickets.push_back(t);
+    }
+
+    void Review() {
+
+        for (int i = 0; i <= tickets.size(); i++) {
+            cout << "Ticket serial: " << tickets[i].ser << endl;
+            cout << "Ticket number: " << tickets[i].ser << endl;
+            cout << "Time:\nyear: " << tickets[i].ser << endl;
+            cout << "month: " << tickets[i].ser << endl;
+            cout << "day: " << tickets[i].ser << endl;
+            cout << "Place departure: " << tickets[i].ser << endl;
+            cout << "Place arrival: " << tickets[i].ser << endl;
+        }
+
+    }
+
+    void Search() {
+
+        int ser, num;
+        cout << "Input ticket serial number >> ";
+        cin >> ser;
+        cout << "Input ticket number >> ";
+        cin >> num;
+
+        for (int i = 0; i <= tickets.size(); i++) {
+            
+            if (tickets[i].ser == ser && tickets[i].num == num) {
+                ReviewOne(i);
+                cout << "---------------------------------\n\n";
+            }
+
+        }
+
+    }
+
+};
+
+int main()
+{
+    AviaTickets a;
+
+    a.Add();
+    a.Add();
+    a.Review();
+    a.Search();
+
+}
